7096b3d912803ded2be2eaab631ceff7870868dc
Eric Hartline <hartline.eric@gmail.com>
2022-11-04 15:45:22 -0500

    set up chakra theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,40 @@
-import '../styles/globals.css'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+import 'reset-css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const theme = extendTheme({
+  colors: {
+    gray: {
+      100: '#F5f5f5',
+      200: '#EEEEEE',
+      300: '#E0E0E0',
+      400: '#BDBDBD',
+      500: '#9E9E9E',
+      600: '#757575',
+      700: '#616161',
+      800: '#424242',
+      900: '#212121',
+    },
+  },
+  components: {
+    Button: {
+      variants: {
+        link: {
+          ':focus': {
+            boxShadow: 'none',
+            outline: 'none',
+          },
+        },
+      },
+    },
+  },
+})
+
+const MyApp = ({ Component, pageProps }) => {
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
