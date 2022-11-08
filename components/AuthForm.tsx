c99904ce3bd0163864468d98f16f18a7f8072ae6
Eric Hartline <hartline.eric@gmail.com>
2022-11-08 15:43:17 -0600

    wire up auth form

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import { Box, Button, Flex, Input } from '@chakra-ui/react'
 import { useRouter } from 'next/router'
+import NextImage from 'next/image'
 import { useSWRConfig } from 'swr'
 import { auth } from '../lib/mutations'
 
 type Props = {
-  mode: string
+  mode: 'signin' | 'signup'
 }
 
 const AuthForm = ({ mode }: Props) => {
@@ -14,13 +15,54 @@ const AuthForm = ({ mode }: Props) => {
   const [isLoading, setIsLoading] = useState(false)
   const router = useRouter()
 
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    setIsLoading(true)
+    await auth(mode, {
+      email,
+      password,
+    })
+    setIsLoading(false)
+    router.push('/')
+  }
+
   return (
     <Box height="100vh" width="100vw" bg="black" color="white">
-      <Flex justify="center" align="center" height="100px">
-        hello
+      <Flex
+        justify="center"
+        align="center"
+        height="100px"
+        borderBottom="white 1px solid"
+      >
+        <NextImage src="/logo.svg" height={60} width={120} />
       </Flex>
       <Flex justify="center" align="center" height="calc(100vh - 100px)">
-        form
+        <Box padding="50px" bg="gray.900" borderRadius="6px">
+          <form onSubmit={handleSubmit}>
+            <Input
+              placeholder="email"
+              type="email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input
+              placeholder="password"
+              typeof="password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button
+              type="submit"
+              bg="green.500"
+              isLoading={isLoading}
+              sx={{
+                '&:hover': {
+                  bg: 'green.300',
+                },
+              }}
+            >
+              {mode}
+            </Button>
+          </form>
+        </Box>
       </Flex>
     </Box>
   )
